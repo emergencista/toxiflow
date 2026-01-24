032ee2a07e71aeba71d6c36dabcbd001d93fac62
baandrade64-byte <ba.andrade.64@gmail.com>
2026-01-23 21:10:50 -0300

    Update database.js

--- database.js
+++ database.js
@@ -1,17 +1,29 @@
 window.database = [
-  { n: "AAS", syn: ["Ácido acetilsalicílico", "Acido acetilsalicilico"], d: 100, unit: "mg", mv: "30 hs", ant: "Bicarbonato de Sódio", support: "Alcalinização urinária." },
-  { n: "Amitriptilina", d: 3, unit: "mg", mv: "25 hs", ant: "Bicarbonato de Sódio", support: "ECG (QRS). Tratar convulsões." },
-  { n: "Atenolol", d: 5, unit: "mg", mv: "7 hs", ant: "Glucagon / Insulina", support: "Monitorar hemodinâmica." },
-  { n: "Carbamazepina", d: 20, unit: "mg", mv: "20 hs", support: "Carvão seriado (MDAC)." },
-  { n: "Clonazepam", d: 0.6, unit: "mg", mv: "40 hs", ant: "Flumazenil (Cuidado)", support: "Suporte respiratório." },
-  { n: "Diazepam", d: 0.7, unit: "mg", mv: "48 hs", ant: "Flumazenil (Cuidado)", support: "Suporte ventilatório." },
-  { n: "Digoxina", d: 20, unit: "mcg", mv: "40 hs", ant: "Fab Anti-Digoxina", support: "Monitorar K+ e ECG." },
-  { n: "Fenobarbital", d: 4, unit: "mg", mv: "5 dias", support: "Alcalinização urinária." },
-  { n: "Lítio", syn: ["Litio", "Carbonato Lítio", "Carbonato Litio", "Lítio (Carbonato)"], d: 50, unit: "mg", mv: "24 hs", noCharcoal: true, ant: "Hemodiálise", support: "Hidratação. Carvão ineficaz." },
-  { n: "Paracetamol", syn: ["Acetaminofeno"], d: 75, unit: "mg", mv: "3 hs", ant: "N-Acetilcisteína", support: "Nomograma em 4h." },
+  { n: "AAS", syn: ["Ácido acetilsalicílico", "Acido acetilsalicilico", "Aspirina"], d: 100, unit: "mg", mv: "30 hs", ant: "Bicarbonato de sódio (alcalinização)", support: "Suporte clínico, correção hidroeletrolítica, considerar carvão ativado precoce se indicado." },
 
-  { n: "Metformina", d: 80, unit: "mg", mv: "6,5 hs" },
-  { n: "Sertralina", d: 7, unit: "mg", mv: "26 hs" },
-  { n: "Haloperidol", syn: ["Haldol"], d: 0.5, unit: "mg", mv: "35 hs" },
-  { n: "Ácido Valpróico", syn: ["Acido valproico", "Valproato"], d: 200, unit: "mg", mv: "20 hs" }
+  { n: "Amitriptilina", syn: ["Tricíclicos", "ADT"], d: 3, unit: "mg", mv: "25 hs", ant: "Bicarbonato de sódio (QRS alargado)", support: "Monitorização contínua, tratar convulsões, suporte ventilatório se necessário." },
+
+  { n: "Atenolol", syn: ["Beta-bloqueador"], d: 5, unit: "mg", mv: "7 hs", ant: "Glucagon / insulina em alta dose (conforme protocolo)", support: "Monitorar FC/PA, ECG, suporte hemodinâmico." },
+
+  { n: "Carbamazepina", syn: ["Tegretol"], d: 20, unit: "mg", mv: "20 hs", support: "Considerar carvão ativado em doses múltiplas (MDAC) conforme avaliação clínica." },
+
+  { n: "Clonazepam", syn: ["Rivotril", "Benzodiazepínico"], d: 0.6, unit: "mg", mv: "40 hs", ant: "Flumazenil (uso selecionado, cautela)", support: "Suporte ventilatório e clínico conforme necessidade." },
+
+  { n: "Diazepam", syn: ["Valium", "Benzodiazepínico"], d: 0.7, unit: "mg", mv: "48 hs", ant: "Flumazenil (uso selecionado, cautela)", support: "Suporte ventilatório e clínico conforme necessidade." },
+
+  { n: "Digoxina", syn: ["Digital", "Lanoxin"], d: 20, unit: "mcg", mv: "40 hs", ant: "Fab anti-digoxina", support: "Monitorar ECG e potássio. Tratar arritmias conforme protocolo." },
+
+  { n: "Fenobarbital", syn: ["Barbitúrico"], d: 4, unit: "mg", mv: "5 dias", support: "Suporte ventilatório. Considerar alcalinização urinária conforme avaliação." },
+
+  { n: "Haloperidol", syn: ["Haldol"], d: 0.5, unit: "mg", mv: "35 hs", support: "Monitorar QT. Tratar distonia/agitação conforme protocolo." },
+
+  { n: "Lítio", syn: ["Litio", "Carbonato de lítio", "Carbonato de litio"], d: 50, unit: "mg", mv: "24 hs", noCharcoal: true, ant: "Hemodiálise (casos selecionados)", support: "Hidratação, monitorar níveis e função renal. Carvão não é eficaz." },
+
+  { n: "Metformina", syn: ["Biguanida"], d: 80, unit: "mg", mv: "6,5 hs", support: "Observar acidose láctica. Suporte hemodinâmico conforme quadro." },
+
+  { n: "Paracetamol", syn: ["Acetaminofeno", "Tylenol"], d: 75, unit: "mg", mv: "3 hs", ant: "N-acetilcisteína", support: "Dosar paracetamol em 4h e aplicar nomograma conforme protocolo." },
+
+  { n: "Sertralina", syn: ["ISRS", "Zoloft"], d: 7, unit: "mg", mv: "26 hs", support: "Suporte clínico. Observar síndrome serotoninérgica." },
+
+  { n: "Ácido Valpróico", syn: ["Acido valproico", "Valproato"], d: 200, unit: "mg", mv: "20 hs", support: "Suporte clínico. Considerar carnitina em situações selecionadas." }
 ];
